d076b00b4cdb40e113e92a864435eb996576c73b
Hoang Tam <fakkukawaiiuwu@gmail.com>
2024-03-26 17:44:06 +0700

    Remove search bar and rewrite some texts

--- resources/views/admin/Website/faculity.blade.php
+++ resources/views/admin/Website/faculity.blade.php
@@ -10,11 +10,11 @@
                 <div class="row align-items-center">
                     <div class="col-md-12">
                         <div class="page-header-title">
-                            <h5 class="m-b-10">Add an event</h5>
+                            <h5 class="m-b-10">Faculity</h5>
                         </div>
                         <ul class="breadcrumb">
                             <li class="breadcrumb-item"><a href="#!"><i class="feather icon-home"></i></a></li>
-                            <li class="breadcrumb-item"><a href="#!">Add an event</a></li>
+                            <li class="breadcrumb-item"><a href="#!">Faculity</a></li>
                         </ul>
                     </div>
                 </div>
