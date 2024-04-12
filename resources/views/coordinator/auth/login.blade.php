17d2a966b38cfd61cf9addd4ef59520b3351fdfe
Hungg Duy Bui <buiduyhungg2310@gmail.com>
2024-04-12 13:42:58 +0700

    update 74

--- resources/views/coordinator/auth/login.blade.php
+++ resources/views/coordinator/auth/login.blade.php
@@ -40,7 +40,7 @@
                                 Sign in
                             </button>
                             <p><a href="{{ route('admin_login') }}">Admin Login</a></p>
-                            <p><a href="{{ route('manager_student') }}">Marketing Manager Login</a></p>
+                            <p><a href="{{ route('manager_login') }}">Marketing Manager Login</a></p>
                             <p><a href="{{ route('student_login') }}">Student Login</a></p>
                         </form>
                     </div>
